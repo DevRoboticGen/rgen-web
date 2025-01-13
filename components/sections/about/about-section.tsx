4925722097459a01cfd48195abdfb0daa86898e2
Deepana Ishtaweera <dishtaweera@gmail.com>
2025-01-14 00:31:01 +0100

    update content

--- components/sections/about/about-section.tsx
+++ components/sections/about/about-section.tsx
@@ -4,7 +4,7 @@ const initiatives = [
     title: "Sri Lanka’s first Industry funded Robotics & AI Lab",
     subtitle: "Our Initiatives",
     description:
-      "launched in May 2023, we’ve embarked on a journey to redefine learning and research in Sri Lanka Launched in May 2023, we’ve embarked on a journey to redefine learning and research in Sri LankaLaunched in May 2023, we’ve embarked on a journey to redefine learning and research in Sri Lanka",
+      "At RoboticGen Labs, our award-winning team focuses on creating innovative solutions in Robotics & AI. Our focus areas include smart mobile robots with perception capabilities, physical AI applications, STEM tools and kits, and industrial automation solutions. We also collaborate with universities on research projects and open-source initiatives.",
     imageSrc: "/images/innovate-text.jpg",
     altText: "Labs",
   },
@@ -12,7 +12,7 @@ const initiatives = [
     title: "The Best Place to build and \nLearn afterschool",
     subtitle: "Our Initiatives",
     description:
-      "launched in May 2023, we’ve embarked on a journey to redefine learning and research in Sri Lanka Launched in May 2023, we’ve embarked on a journey to redefine learning and research in Sri LankaLaunched in May 2023, we’ve embarked on a journey to redefine learning and research in Sri Lanka",
+      "At RoboticGen Academy, we're revolutionizing education through our innovative journey-based learning approach! Our passionate mentors guide children through engaging, self-paced projects aligned with Sustainable Development Goals - from core foundations to exciting paths in Robotics, Programming, and AI. Regular team challenges spark creativity and build essential life skills.",
     imageSrc: "/images/learn-text.png",
     altText: "Academy",
   },
